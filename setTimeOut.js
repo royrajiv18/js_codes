61f14ba04f66c2ed1ee651a1d29b852231517cfa
Rajiv Roy <rajivpune18@gmail.com>
2025-02-27 16:50:56 +0530

    print 1 to 5 after each second

---
+++ setTimeOut.js
@@ -0,0 +1,20 @@
+function x() {
+  for (var i = 1; i <= 5; i++) {
+    function close(x) {
+      setTimeout(() => {
+        console.log(x);
+      }, x * 1000);
+    }
+    close(i);
+  }
+}
+
+x();
+
+/*
+1
+2
+3
+4
+5
+*/
