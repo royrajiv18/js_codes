eb75b76361b35f690d42f6d585f51938b5f542dc
Rajiv Roy <rajivpune18@gmail.com>
2025-03-03 13:14:03 +0530

    this inside arrow fn

--- this.js
+++ this.js
@@ -41,3 +41,54 @@ const obj1 = {
 obj1.x();
 
 // 10
+
+// **************************************
+
+//Arrow function doesn't have its own "this".
+//They take the value of the lexical environment where they are enclosed.
+const a = (x) => {
+  console.log(this);
+};
+a();
+// window object
+
+// **************************************
+
+const obj2 = {
+  a: 10,
+  x: () => {
+    console.log(this);
+  },
+};
+obj2.x();
+
+// window object
+
+// **************************************
+// inside nested arrow fn
+
+const obj3 = {
+  a: 10,
+  x: function () {
+    const y = () => {
+      console.log(this);
+    };
+    y();
+  },
+};
+obj3.x();
+
+// {a: 10, x: fn}
+// this takes the value of its parents lexical env
+
+// **************************************
+
+// this inside dom refer to the html element
+
+/*
+<button onclick="alert(this)">
+Click me
+<button/>
+*/
+
+// [object HTMLButtonELement]
